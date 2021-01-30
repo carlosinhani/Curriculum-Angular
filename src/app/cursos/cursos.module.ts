cdb2bb792ce8d8c3baa8f009560691ebee40c035
Carlos.inhani <carlos.inhani@gmail.com>
2021-01-30 08:11:03 -0300

    register

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
 import { CursosComponent } from './cursos/cursos.component';
+import { CadastroCursosComponent } from './cadastro-cursos/cadastro-cursos.component';
 
 
 @NgModule({
-  declarations: [CursosComponent],
+  declarations: [
+    CursosComponent,
+    CadastroCursosComponent
+  ],
   imports: [
     CommonModule,
     CursosRoutingModule
