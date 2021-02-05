2753f94ec9c5669de3bf2f755d71db1993dd119e
Carlos.inhani <carlos.inhani@gmail.com>
2021-02-05 16:25:51 -0300

    project-list

--- src/app/projetos/projetos-routing.module.ts
+++ src/app/projetos/projetos-routing.module.ts
@@ -1,12 +1,12 @@
+import { ProjectListComponent } from './project-list/project-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CommitProjComponent } from './commit-proj/commit-proj.component';
 
 const routes: Routes = [
-  {
-    path: '', component: CommitProjComponent
-  }
+  {path: '', component: ProjectListComponent},
+  {path: 'cadastro', component: CommitProjComponent}
 ];
 
 @NgModule({
--- src/app/projetos/projetos.module.ts
+++ src/app/projetos/projetos.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProjetosRoutingModule } from './projetos-routing.module';
 import { CommitProjComponent } from './commit-proj/commit-proj.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 
 @NgModule({
-  declarations: [CommitProjComponent],
+  declarations: [CommitProjComponent, ProjectListComponent],
   imports: [
     CommonModule,
     ProjetosRoutingModule
