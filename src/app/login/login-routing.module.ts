8c2df0ee1ffd458139cdaa1cfb6a0b04d5255b0c
Carlos.inhani <carlos.inhani@gmail.com>
2021-09-05 21:35:28 -0300

    login_button

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,12 +1,16 @@
 import { LoginComponent } from './login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../curriculum/home/home.component';
 
 
 
 const routes: Routes = [
   {
     path: '', component: LoginComponent
+  },
+  {
+    path: 'curriculum', component: HomeComponent
   }
 ];
 
